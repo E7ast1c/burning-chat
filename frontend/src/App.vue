ba8148c32292454459d99450a4a791e7c3b3a93e
Артем Корольков <aikorolkov@vtb.ru>
2022-02-17 01:10:26 +0300

    added fonts & basic styles

---
+++ frontend/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+<!--  <div id="nav">-->
+<!--    <router-link to="/">Chat</router-link> |-->
+<!--    <router-link to="/about">About</router-link>-->
+<!--  </div>-->
+  <div class="container">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+@font-face {
+  font-family: 'Source Code Pro', monospace;
+  src: url('./assets/fonts/SourceCodePro-Regular.ttf') format('woff');
+  font-style: normal;
+  font-weight: 400;
+}
+@font-face {
+  font-family: 'Source Code Pro', monospace;
+  src: url('./assets/fonts/SourceCodePro-Medium.ttf') format('woff');
+  font-style: normal;
+  font-weight: 500;
+}
+@font-face {
+  font-family: 'Source Code Pro', monospace;
+  src: url('./assets/fonts/SourceCodePro-SemiBold.ttf') format('woff');
+  font-style: normal;
+  font-weight: 600;
+}
+*{
+  background: #0f0f23;
+  box-sizing: border-box;
+  margin: 0;
+}
+
+body{
+  font-size: 62.5%;
+}
+#app {
+  font-family: "Source Code Pro", monospace;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+.container{
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-column-gap: 2.4rem;
+}
+</style>
---
+++ frontend/src/views/Chat.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="terminal">
+    <header class="header">
+      <button class="header-btn">[Create Chat]</button>
+      <button class="header-btn">[Connect To Chat]</button>
+      <button class="header-btn">[Settings]</button>
+    </header>
+    <div v-if="isVueChat" class="content">
+      <div class="window-chat">
+        <div class="answer-mess"></div>
+        <div class="mess"></div>
+      </div>
+      <input type="text" class="input-mess">
+      <button class="btn">Send message</button>
+    </div>
+  </section>
+</template>
+
+<script>
+const isVueChat = false;
+</script>
+
+<style>
+.terminal{
+  grid-column: 1 / span 12;
+  height: 100vh;
+  color: green;
+  padding: 2em 1em;
+}
+.header{
+  display: flex;
+}
+.header-btn{
+  color: #009900;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+  font-weight: 500;
+  font-size: 1.8em;
+  padding: .1rem .1rem;
+  font-family: "Source Code Pro", monospace;
+  margin-right: 1rem;
+}
+
+.window-chat{
+  width: 80%;
+  height: 60%;
+  background: #ccc;
+}
+.input-mess{
+  border: none;
+  border-bottom: 1px solid white;
+  outline: none;
+  color: #009900;
+}
+.btn{
+  color: #009900;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+}
+</style>
